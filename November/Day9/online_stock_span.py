c83b95cbc38bceaaa787585b30427b74642ffe0a
Abdullah <48406637+shy-tan@users.noreply.github.com>
2022-11-09 21:11:21 +0500

    add stack sol

--- November/Day9/online_stock_span.py
+++ November/Day9/online_stock_span.py
@@ -20,3 +20,31 @@ class StockSpanner:
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
+
+
+# using stack
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        if not self.stack:
+            item = (price, 1)
+            self.stack.append(item)
+            return 1
+        
+        count = 1
+        
+        while self.stack and self.stack[-1][0] <= price:
+            print('run')
+            _, oldCount = self.stack.pop()
+            count += oldCount
+        
+        newItem = (price, count)
+        self.stack.append(newItem)
+        
+        return count
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
