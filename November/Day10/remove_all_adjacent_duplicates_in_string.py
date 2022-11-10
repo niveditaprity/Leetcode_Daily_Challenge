ea33c3d1dc2389cd492ebaa7fd2f1d07b8035361
Abdullah <48406637+shy-tan@users.noreply.github.com>
2022-11-10 12:05:43 +0500

    add optimal solution

--- November/Day10/remove_all_adjacent_duplicates_in_string.py
+++ November/Day10/remove_all_adjacent_duplicates_in_string.py
@@ -28,3 +28,21 @@ class Solution:
             out, flg = helper(out)
         
         return out
+
+# solution using stack
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+
+        stack = [s[0]]
+
+        for i in range(1, len(s)):
+            if stack and stack[-1] == s[i]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+        
+        s = "".join(stack)
+
+        return s
