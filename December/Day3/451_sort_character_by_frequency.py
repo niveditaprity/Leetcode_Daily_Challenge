92d3c09eb07fd7266f96f01d8ba69229e5d08391
mohitCodepy <mohit1511199@gmail.com>
2022-12-03 16:19:42 +0530

    sort characters by frequency without Counter

--- December/Day3/451_sort_character_by_frequency.py
+++ December/Day3/451_sort_character_by_frequency.py
@@ -10,3 +10,22 @@ class Solution:
 class Solution:
     def frequencySort(self, s: str) -> str:
         return "".join([i for i,v in Counter(s).most_common() for j in range(v)])
+
+
+# without using Counter
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        char_freq = {}
+        for i in s:
+            if i not in char_freq:
+                char_freq[i] = 1
+            else:
+                char_freq[i] += 1
+
+        sorted_by_freq = sorted(char_freq.items(), key=lambda x: x[1], reverse=True)
+        
+        sorted_string = ""
+        for ch, fq in sorted_by_freq:
+            for j in range(fq):
+                sorted_string += ch
+        return sorted_string
