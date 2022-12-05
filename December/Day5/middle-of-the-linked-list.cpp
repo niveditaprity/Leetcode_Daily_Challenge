2cae27a0677b92e130b6673dec08f6935c7ea7dd
Shamikh05 <shamiikhullah.akram18@gmail.com>
2022-12-05 11:18:24 +0530

    Using slow and fast pointers. O(N) Time Complexity. O(1) Space Complexity.

---
+++ December/Day5/middle-of-the-linked-list.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+};
