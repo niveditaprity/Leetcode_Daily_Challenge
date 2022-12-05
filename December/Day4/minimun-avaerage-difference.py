ea92146f9f064d53c2462deb39d348a7becd8243
mohitCodepy <mohit1511199@gmail.com>
2022-12-05 15:45:18 +0530

    mininum average difference using map

--- December/Day4/minimun-avaerage-difference.py
+++ December/Day4/minimun-avaerage-difference.py
@@ -18,3 +18,22 @@ class Solution:
                 min_avg = abs_diff
                 min_key = i
         return min_key
+
+# using extra space (map)
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        d = {}
+        total_sum = sum(nums)
+        presum = 0
+        len_n = len(nums)
+        for i, v in enumerate(nums):
+            presum += v
+            ab1 = presum//(i+1)
+            divs = len_n-i-1
+            if divs <=0:
+                ab2 = total_sum - presum
+            else:
+                ab2 = (total_sum - presum)// divs
+
+            d.update({i: abs(ab1 - ab2)})
+        return min(d.items(), key=lambda x: x[1])[0]
