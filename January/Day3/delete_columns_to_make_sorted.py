f5cc0a4cdf9b6d34feaa20a7caebc95d66f227c7
mohitCodepy <mohit1511199@gmail.com>
2023-01-03 23:00:32 +0530

    delete columns to make sorted without using array

--- January/Day3/delete_columns_to_make_sorted.py
+++ January/Day3/delete_columns_to_make_sorted.py
@@ -10,3 +10,15 @@ class Solution:
             if sorted(s) != s:
                 count += 1
         return count
+
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+
+        count = 0
+        for row in range(len(strs[0])):
+            for col in range(1, len(strs)):
+                if strs[col][row] < strs[col-1][row]:
+                    count += 1
+
+        return count
