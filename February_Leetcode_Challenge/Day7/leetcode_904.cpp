4cf467502394704df3c079a796b69ee88ad5fd3e
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2023-02-07 10:26:13 +0530

    Create leetcode_904.cpp

---
+++ February_Leetcode_Challenge/Day7/leetcode_904.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+      int ans = 0;
+      map<int,int>mp;
+      int i =0 ,j =0,n = fruits.size();
+      while(i<n)
+      {
+        mp[fruits[i]]++;
+      while(j<n && mp.size()>2)
+      {
+       if(mp[fruits[j]] == 1)
+       {
+        mp.erase(fruits[j]);
+       }
+        else
+        {
+        mp[fruits[j]]--;
+        }
+        j++;
+      }
+       ans = max(i-j+1,ans);
+       i++;
+    }
+    return ans;
+    }
+};
